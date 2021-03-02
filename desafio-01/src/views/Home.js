7da758622890e404bbd766b7861d49244ddb169a
Felipe Oliveira <felipe.boliveira@outlook.com>
2021-03-02 19:59:49 -0300

    Implementa ordernacao por produtos acessados

--- desafio-01/src/views/Home.js
+++ desafio-01/src/views/Home.js
@@ -6,40 +6,44 @@ import 'react-toastify/dist/ReactToastify.css'
 import api from '../services/api'
 import '../styles/style.css'
 
-const productsMockColumn = [
-    {
-        dataField: 'id',
-        text: '#',
-        headerStyle: (colum, colIndex) => {
-            return { width: '10vw' }
-        },
-    },
-    {
-        dataField: 'nome',
-        text: 'Produto',
-    },
-]
-
 export default function Home() {
     const [text, setText] = useState('')
     const [products, setProducts] = useState('')
 
     async function fetchSearchedProducts() {
         try {
-            const response = await api.get(`${text}&source=nanook`)
-            setProducts(
-                response.data.products.map((p) => {
-                    const formatedProducts = {
-                        id: p.id,
-                        nome: p.name,
-                    }
-                    return formatedProducts
-                })
-            )
+            if (text && text.length >= 3) {
+                const query = text.toLocaleLowerCase()
+                const response = await api.get(`${query}&source=nanook`)
+                setProducts(formatProductList(response.data.products))
+            } else {
+                toast.warn('A busca deve conter mais de 3 caracteres')
+            }
         } catch (err) {
             toast.warn('Não foi possível carregar a lista de produtos')
         }
     }
+
+    const formatProductList = (unformatedList) => {
+        let teste = unformatedList.map((p) => {
+            const formatedProducts = {
+                id: p.id,
+                name: p.name,
+                visitsClickCount: p._meta.visitsClickCount,
+            }
+            return formatedProducts
+        })
+        return orderByVisitsClickCount(teste)
+    }
+
+    const orderByVisitsClickCount = (unorderedList) => {
+        const teste = unorderedList.sort((a, b) => {
+            return b.visitsClickCount - a.visitsClickCount
+        })
+        return teste
+    }
+
+    console.log(products)
 
     return (
         <div className='homeContainer'>
@@ -75,7 +79,7 @@ export default function Home() {
                             placeholder='Digite o nome do produto'
                             keyField='id'
                             data={products}
-                            columns={productsMockColumn}
+                            columns={[{ dataField: 'name', text: 'Produto' }]}
                             noDataIndication='Nenhuma informação encontrada'
                         ></BootstrapTable>
                     </Col>
